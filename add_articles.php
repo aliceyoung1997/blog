9ff88b6a2cfa9202ae6062b01b4f08de17e70750
aliceyoung <alice06181997@gmail.com>
2021-10-24 15:21:16 +0800

    modified path

--- add_articles.php
+++ add_articles.php
@@ -14,8 +14,7 @@ if(empty($_SESSION['username'])){
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Edit</title>
-  <!-- <link rel="stylesheet" href="./stylesheet.css" /> -->
-  <link rel="stylesheet" href="./main.css" />
+  <link rel="stylesheet" href="./css/main.css" />
   <link rel="stylesheet" href="https://necolas.github.io/normalize.css/8.0.1/normalize.css" />
 </head>
 
